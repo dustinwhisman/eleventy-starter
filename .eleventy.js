3a51e4c1d7b5b3f4ff232631dcaf6ab5ba11e755
Dustin Whisman <whisman.dustin@gmail.com>
2021-06-23 10:00:09 -0600

    refactor: move pages into their own folder
    To separate things like scss, scripts, and components from pages,
    configure eleventy to use the `pages` folder as its input.
    
    Do some light refactoring of the 404 page and the stack
    component.

--- .eleventy.js
+++ .eleventy.js
@@ -21,7 +21,8 @@ module.exports = function(eleventyConfig) {
 
   return {
     dir: {
-      input: 'src',
+      includes: '../_includes',
+      input: 'src/pages',
       output: 'dist',
     },
   };
