02b16e7d197a5d69c7e43b52c6d90175e7c397e7
dustin-jw <dustin@heysparkbox.com>
2021-09-09 10:30:22 -0600

    feat: support automatic bundling/minification/transpiling of JS
    chore: set node engine minimum to the oldest LTS version
    
    feat: add basic JS bundling with Rollup
    
    Add Rollup config and npm commands to dynamically bundle any JS in the
    `src/assets/js` folder, ignoring subfolders.
    
    Add some example scripts and a page to describe how to work with JS.
    
    feat: support non-bundled scripts
    
    Update configs to copy JS source code to `dist/js`, and update examples and docs
    to explain how that works.
    
    feat: minify bundled JS using `rollup-plugin-terser`
    
    feat: minify JS files before putting them in `dist`
    
    feat: support legacy JS bundles
    
    docs: document how to work with JS

---
+++ src/assets/js/example-2.mjs
@@ -0,0 +1,5 @@
+import { add } from './utilities/add.js';
+import { subtract } from './utilities/subtract.js';
+
+console.log(add(4, 4));
+console.log(subtract(5, 2));
---
+++ src/assets/js/example.js
@@ -0,0 +1,5 @@
+import { add } from './utilities/add';
+import { subtract } from './utilities/subtract';
+
+console.log(add(2, 2));
+console.log(subtract(3, 1));
