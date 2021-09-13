fb11744900a0c2ffdc3fdb886fbef6f8c85ff911
dustin-jw <dustin@heysparkbox.com>
2021-09-13 08:21:32 -0600

    feat: enhance layout templates with blocks to allow page-level overrides
    feat: wrap title and description tags in block so they can be extended
    
    By default, the title and descriptions should respect values set in
    page-level front matter. This work eliminates front matter in layouts
    that can cause issues when trying to `extend` layouts.
    
    feat: wrap main body content in block so it can optionally be extended
    
    feat: wrap footer in block so it can be optionally extended
    
    feat: add empty blocks for styles and scripts that will be set at the page level
    
    refactor: delete redundant docs layout template
    
    docs: add docs page for templates and using blocks
    
    feat: update javascript docs page to use scripts block

--- src/assets/js/example-2.mjs
+++ src/assets/js/example-2.mjs
@@ -1,5 +1,6 @@
 import { add } from './utilities/add.js';
 import { subtract } from './utilities/subtract.js';
 
-add(4, 4); // 8
-subtract(5, 2); // 3
+/* eslint-disable no-console */
+console.log(add(4, 4)); // 8
+console.log(subtract(5, 2)); // 3
--- src/assets/js/example.js
+++ src/assets/js/example.js
@@ -1,5 +1,6 @@
 import { add } from './utilities/add.js';
 import { subtract } from './utilities/subtract.js';
 
-add(2, 2); // 4
-subtract(3, 1); // 2
+/* eslint-disable no-console */
+console.log(add(2, 2)); // 4
+console.log(subtract(3, 1)); // 2
