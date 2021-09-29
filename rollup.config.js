6bfac4996994358e50ff5c7afd703a4694bc1ea1
dustin-jw <dustin@heysparkbox.com>
2021-09-29 10:02:34 -0600

    feat: install client-side firebase and update configs
    - Install firebase and use environment variables to make it easier to
      update for different projects
    - Update rollup config so it can import from node_modules
    - Update rollup config to replace environment variables with the real
      values

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,22 @@
+import dotenv from 'dotenv';
 import fs from 'fs';
+import replace from '@rollup/plugin-replace';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 import { babel } from '@rollup/plugin-babel';
+
+dotenv.config();
+
+const replaceOptions = {
+  values: {
+    'process.env.FIREBASE_API_KEY': process.env.FIREBASE_API_KEY,
+    'process.env.FIREBASE_PROJECT_ID': process.env.FIREBASE_PROJECT_ID,
+    'process.env.FIREBASE_MESSAGING_SENDER_ID': process.env.FIREBASE_MESSAGING_SENDER_ID,
+    'process.env.FIREBASE_APP_ID': process.env.FIREBASE_APP_ID,
+  },
+  preventAssignment: true,
+  delimiters: ['', ''],
+};
 
 const files = fs.readdirSync('src/assets/js').filter((file) => file.endsWith('.js') || file.endsWith('.mjs'));
 const configs = [];
@@ -23,6 +39,8 @@ files.forEach((file) => {
       format: 'iife',
     },
     plugins: [
+      nodeResolve(),
+      replace(replaceOptions),
       terser(),
     ],
   });
@@ -35,6 +53,8 @@ files.forEach((file) => {
       format: 'iife',
     },
     plugins: [
+      nodeResolve(),
+      replace(replaceOptions),
       babel({
         presets: ['@babel/preset-env'],
         babelHelpers: 'bundled',
---
+++ src/assets/js/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'process.env.FIREBASE_API_KEY',
+  authDomain: 'process.env.FIREBASE_PROJECT_ID.firebaseapp.com',
+  projectId: 'process.env.FIREBASE_PROJECT_ID',
+  storageBucket: 'process.env.FIREBASE_PROJECT_ID.appspot.com',
+  messagingSenderId: 'process.env.FIREBASE_MESSAGING_SENDER_ID',
+  appId: '1:process.env.FIREBASE_MESSAGING_SENDER_ID:web:process.env.FIREBASE_APP_ID'
+};
+
+// Initialize Firebase
+export const firebase = initializeApp(firebaseConfig);
