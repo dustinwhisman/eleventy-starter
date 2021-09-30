24a7713bc1cc4eee18d13152eea2f1eb087dbe1e
dustin-jw <dustin@heysparkbox.com>
2021-09-30 11:25:14 -0600

    feat: allow users to delete their accounts
    Write the functions and user flow for users to securely delete their
    accounts.

---
+++ src/assets/js/delete-account.js
@@ -0,0 +1,48 @@
+import { observeAuthState } from './utilities/auth/observe-auth-state.js';
+import { reauthenticateUser } from './utilities/auth/reauthenticate-user.js';
+import { deleteAccount } from './utilities/auth/delete-account.js';
+
+(() => {
+  let isLoggedIn = false;
+  let isDeleted = false;
+
+  const revealElement = (selector) => {
+    const element = document.querySelector(selector);
+    element.removeAttribute('hidden');
+  };
+
+  const handleLoggedInEvent = () => {
+    // prevent reauthentication from triggering this again
+    if (isLoggedIn) {
+      return;
+    }
+
+    isLoggedIn = true;
+
+    reauthenticateUser(
+      () => { revealElement('[data-delete-account]') },
+      () => { revealElement('[data-reauthentication-failure]') },
+    );
+  };
+
+  const handleLoggedOutEvent = () => {
+    if (!isDeleted) {
+      revealElement('[data-logged-out]');
+    }
+  };
+
+  document.addEventListener('user-logged-in', handleLoggedInEvent);
+  document.addEventListener('user-logged-out', handleLoggedOutEvent);
+
+  document.addEventListener('click', (event) => {
+    if (event.target.matches('[data-delete-account-button]')) {
+      isDeleted = true;
+      deleteAccount(
+        () => { revealElement('[data-success-message]') },
+        () => { revealElement('[data-failure-message]') },
+      );
+    }
+  });
+
+  observeAuthState();
+})();
---
+++ src/assets/js/utilities/auth/delete-account.js
@@ -0,0 +1,9 @@
+import { deleteUser } from '@firebase/auth';
+import { auth } from './auth.js';
+
+export const deleteAccount = (handleSuccess, handleError) => {
+  const user = auth.currentUser;
+  deleteUser(user)
+    .then(handleSuccess)
+    .catch(handleError);
+};
